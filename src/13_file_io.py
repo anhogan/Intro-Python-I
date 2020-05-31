50777df12eecbc964bc456170526ed00b7863429
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-05-31 16:28:12 -0400

    Implement file and scope files solutions

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,12 +8,17 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
-
-# YOUR CODE HERE
+t = open('./src/foo.txt')
+print(t.read())
+t.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
+n = open('./src/bar.txt', 'w')
+n.write("This is the first line\nThis is the second line\nThis is the third line")
+n.close()
 
-# YOUR CODE HERE
+o = open('./src/bar.txt')
+print(o.read())
